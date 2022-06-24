0338a9cb15c816cd470fc42421e885e42567a2ae
rohit.kumar <rohkumar@rhrk.uni-kl.de>
2022-06-24 17:54:58 +0200

    intial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Covid-19 data analysis- year 2019-2022',
+    author='Rohit Kumar',
+    license='',
+)
